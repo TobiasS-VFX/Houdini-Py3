b7dd41b04c9198ae5d20875113bf2e9be6eee549
Tobias Steiner <82468431+TobiasS-VFX@users.noreply.github.com>
2022-03-24 13:32:25 -0700

    Update MTLX_Builder.py
    * Added metlaness
    * Added color signature settings on image nodes
    * Set flags properly for autofill up stream
    * After initial library creation, rerunning the script will just create new node sets within the library instead of creating new libraries.

--- MTLX_Builder.py
+++ MTLX_Builder.py
@@ -17,6 +17,7 @@ mtlxSurf = mLib_node.createNode('mtlxsurfacematerial')
 mtlxStdSurf = mLib_node.createNode('mtlxstandard_surface')
 mtlxDisp = mLib_node.createNode('mtlxdisplacement')
 albedo_img = mLib_node.createNode('mtlximage', 'Albedo')
+metal_img = mLib_node.createNode('mtlximage', 'Metalness')
 rough_img = mLib_node.createNode('mtlximage', 'Roughness')
 opacity_img = mLib_node.createNode('mtlximage', 'Opacity')
 normal_img = mLib_node.createNode('mtlximage', 'Normal')
@@ -27,11 +28,25 @@ displace_img = mLib_node.createNode('mtlximage', 'Displacement')
 mtlxSurf.setInput(0, mtlxStdSurf, 0)
 mtlxSurf.setInput(1, mtlxDisp, 0)
 mtlxStdSurf.setInput(1, albedo_img,0)
+mtlxStdSurf.setInput(3, metal_img,0)
 mtlxStdSurf.setInput(6, rough_img,0)
 mtlxStdSurf.setInput(38, opacity_img,0)
 mtlxStdSurf.setInput(40, normal_img,0)
 mtlxDisp.setInput(0, displace_img,0)
 
+# set parms
+
+albedo_img.parm('signature').set('color3')
+metal_img.parm('signature').set('float')
+rough_img.parm('signature').set('float')
+opacity_img.parm('signature').set('float')
+normal_img.parm('signature').set('color3')
+displace_img.parm('signature').set('float')
+
+# set flags
+
+mtlxStdSurf.setMaterialFlag(False)
+mtlxDisp.setMaterialFlag(False)
 # organize layout
 
 mLib_node.layoutChildren()
